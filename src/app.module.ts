553421e3911a1f56289c29b0e8fd64a4e1559a6e
Salcedo <d.s.022222@gmail.com>
2023-07-15 19:17:46 +0200

    Terminando de enlazar la bdd y preparao pa meter la lógica😈

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, UserModule, BookmarkModule],
+  imports: [UserModule, UserModule, BookmarkModule, PrismaModule],
   controllers: [AuthController],
   providers: [AuthService],
 })

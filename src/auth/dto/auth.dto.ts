9b20eb4a766bccdd90524780683dfb0f372085f1
Salsedini <d.s.022222@gmail.com>
2023-08-24 18:27:52 +0200

    Encapsulando la bisnes loyic de auth

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,6 +1,11 @@
-export interface AuthDto{
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
+export class AuthDto {
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
-    password: string;
 
+    @IsString()
+    @IsNotEmpty()
+    password: string;
 }
